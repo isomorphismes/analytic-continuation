c5732bd1ba8a432c6da414ee08ffa2c5735f1612
i <isomorphisms@sdf.org>
2026-08-21 20:01:22 -0400

    Add native convergence explorer: add android/app/src/main/java/org/isomorphisms/analyticcontinuation/ExplorerActivity.java

---
+++ android/app/src/main/java/org/isomorphisms/analyticcontinuation/ExplorerActivity.java
@@ -0,0 +1,7 @@
+package org.isomorphisms.analyticcontinuation;
+
+import android.app.NativeActivity;
+
+/** Hosts the native EGL/OpenGL ES convergence-disc explorer. */
+public final class ExplorerActivity extends NativeActivity {
+}
